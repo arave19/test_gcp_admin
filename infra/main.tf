10c21f483ec4ca5cbc6c83982bdda76a087972d3
arave19 <andres.rave@aravel.one>
2025-10-26 03:03:16 -0500

    modificación en lb y en cloud armor por error.

--- infra/main.tf
+++ infra/main.tf
@@ -27,94 +27,98 @@ resource "google_artifact_registry_repository" "docker_repo" {
   depends_on = [google_project_service.artifactregistry]
 }
 
-resource "google_compute_security_policy" "armor_policy" {
-  project = var.project
-  name = "block-ip-policy"
+# ---------------------------------------------
+# Todo lo siguiente se comenta porque no lo necesitamos
+# para acceso público sin restricciones de IP ni LB
+# ---------------------------------------------
 
-  rule {
-    action = "deny(403)"
-    description = "Restricted ip address"
-    priority = 1000
-    match {
-      versioned_expr = "SRC_IPS_V1"
-      config {
-        src_ip_ranges = [var.ip_restriction]
-      }
-    }
-  }
+# resource "google_compute_security_policy" "armor_policy" {
+#   project = var.project
+#   name = "block-ip-policy"
+#
+#   rule {
+#     action = "deny(403)"
+#     description = "Restricted ip address"
+#     priority = 1000
+#     match {
+#       versioned_expr = "SRC_IPS_V1"
+#       config {
+#         src_ip_ranges = [var.ip_restriction]
+#       }
+#     }
+#   }
+#
+#   rule {
+#     action = "allow"
+#     priority = 2147483647
+#     match {
+#       versioned_expr = "SRC_IPS_V1"
+#       config {
+#         src_ip_ranges = ["*"]
+#       }
+#     }
+#     description = "Default allow rule"
+#   }
+#
+#   depends_on = [google_project_service.compute]
+# }
+#
+# resource "google_compute_region_network_endpoint_group" "cloud_run_neg" {
+#   name                  = "cloud-run-neg"
+#   project               = var.project
+#   region                = var.region
+#   network_endpoint_type = "SERVERLESS"
+#
+#   cloud_run {
+#     service = "simple-webapp-flask"
+#   }
+# }
+#
+# resource "google_compute_backend_service" "cloud_run_backend" {
+#   name        = "cloud-run-backend"
+#   project     = var.project
+#   protocol    = "HTTP"
+#   timeout_sec = 30
+#
+#   backend {
+#     group = google_compute_region_network_endpoint_group.cloud_run_neg.id
+#   }
+#
+#   security_policy = google_compute_security_policy.armor_policy.id
+# }
+#
+# resource "google_compute_url_map" "lb_url_map" {
+#   name            = "cloud-run-url-map"
+#   default_service = google_compute_backend_service.cloud_run_backend.id
+# }
+#
+# resource "google_compute_managed_ssl_certificate" "lb_ssl" {
+#   name = "cloud-run-lb-cert"
+#
+#   managed {
+#     domains = ["arav-el.one"]
+#   }
+# }
+#
+# resource "google_compute_target_https_proxy" "https_proxy" {
+#   name            = "cloud-run-https-proxy"
+#   url_map         = google_compute_url_map.lb_url_map.id
+#   ssl_certificates = [google_compute_managed_ssl_certificate.lb_ssl.name]
+# }
+#
+# resource "google_compute_global_address" "lb_ip" {
+#   name    = "cloud-run-lb-ip"
+#   project = var.project
+# }
+#
+# resource "google_compute_global_forwarding_rule" "https_forwarding_rule" {
+#   name        = "cloud-run-https-forwarding-rule"
+#   target      = google_compute_target_https_proxy.https_proxy.id
+#   port_range  = "443"
+#   ip_protocol = "TCP"
+#   ip_address  = google_compute_global_address.lb_ip.address
+# }
 
-  rule {
-    action = "allow"
-    priority = 2147483647
-    match {
-      versioned_expr = "SRC_IPS_V1"
-      config {
-        src_ip_ranges = ["*"]
-      }
-    }
-    description = "Default allow rule"
-  }
-
-  depends_on = [google_project_service.compute]
-}
-
-resource "google_compute_region_network_endpoint_group" "cloud_run_neg" {
-  name                  = "cloud-run-neg"
-  project               = var.project
-  region                = var.region
-  network_endpoint_type = "SERVERLESS"
-
-  cloud_run {
-    service = "simple-webapp-flask"
-  }
-}
-
-resource "google_compute_backend_service" "cloud_run_backend" {
-  name        = "cloud-run-backend"
-  project     = var.project
-  protocol    = "HTTP"
-  timeout_sec = 30
-
-  backend {
-    group = google_compute_region_network_endpoint_group.cloud_run_neg.id
-  }
-
-  security_policy = google_compute_security_policy.armor_policy.id
-}
-
-resource "google_compute_url_map" "lb_url_map" {
-  name            = "cloud-run-url-map"
-  default_service = google_compute_backend_service.cloud_run_backend.id
-}
-
-resource "google_compute_managed_ssl_certificate" "lb_ssl" {
-  name = "cloud-run-lb-cert"
-
-  managed {
-    domains = ["arav-el.one"]
-  }
-}
-
-resource "google_compute_target_https_proxy" "https_proxy" {
-  name            = "cloud-run-https-proxy"
-  url_map         = google_compute_url_map.lb_url_map.id
-  ssl_certificates = [google_compute_managed_ssl_certificate.lb_ssl.name]
-}
-
-resource "google_compute_global_address" "lb_ip" {
-  name    = "cloud-run-lb-ip"
-  project = var.project
-}
-
-resource "google_compute_global_forwarding_rule" "https_forwarding_rule" {
-  name        = "cloud-run-https-forwarding-rule"
-  target      = google_compute_target_https_proxy.https_proxy.id
-  port_range  = "443"
-  ip_protocol = "TCP"
-  ip_address  = google_compute_global_address.lb_ip.address
-}
-
-# Trigger de Cloud Build (comentado)
 # resource "google_cloudbuild_trigger" "github_push_trigger" {
 #   project  = var.project
 #   location = "global" 
